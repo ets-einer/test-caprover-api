db8c37fc02ef0f82b59f6ab8955c86195de966d6
abehidek <hidek.abe@outlook.com>
2023-10-04 15:15:56 -0300

    feat: add post create end

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import { prisma } from './prisma';
+import { z } from 'zod'
 
 const PORT = 3000
 
@@ -10,6 +11,27 @@ app.get("/bigas", async (req, res) => {
   return res.json({
     message: "Bigas é um bokinha dedo no cu e gritaria () => {}",
     examples
+  })
+})
+
+app.post("/bigas", async (req, res) => {
+  const result = z.object({
+    name: z.string().min(1)
+  }).safeParse(req.body)
+
+  if (!result.success) return res.status(400).json({
+    message: "VC EH BUIRRO"
+  })
+
+  const created = await prisma.example.create({
+    data: {
+      name: result.data.name
+    }
+  })
+
+  return res.json({
+    message: "Bigas criado",
+    created
   })
 })
 
